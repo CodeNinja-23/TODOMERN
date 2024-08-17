290a3d343e2751648cddf6d5e70aa46fc6882c9d
K. Santosh Kumar <107230278+CodeNinja-23@users.noreply.github.com>
2024-08-17 16:49:48 +0530

    Update todoRoutes.js

--- todoRoutes.js
+++ todoRoutes.js
@@ -15,3 +15,14 @@ router.post('/',async(req,res)=>{
     })
     const savedTodo = await newTodo.save();
 })
+router.put('/:id', async(req,res)=>{
+    const UpdatedTodo = await Todo.findByIdAndUpdate(req.params.id,req.body,{new:true});
+    res.json(updatedTodo);
+})
+
+router.delete('/:id', async(req,res)=>{
+    await Todo.findByIdAndDelete(req.params.id);
+    res.json({message:'Todo deleted'})
+})
+
+module.exports=router;
